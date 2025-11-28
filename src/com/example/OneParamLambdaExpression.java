0776c7a5fa459979e1f9025c0a8e0863c6287605
yamini4 <yamini4.g@gmail.com>
2025-11-28 14:49:13 +0530

    adding changes

--- src/com/example/OneParamLambdaExpression.java
+++ src/com/example/OneParamLambdaExpression.java
@@ -1,16 +1,30 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class OneParamLambdaExpression {
+
     interface MultiplyByFive {
         int multBy5(int n);
     }
 
+    interface AddTwoValues {
+        public int addParameters(int a, int b);
+    }
+
     // static MultiplyByFive iMultiplyByFive() {
-    //     return val -> val * 5;
+    // return val -> val * 5;
     // }
+
+    public static int sumUsingStreams(int[] arr) {
+        return Arrays.stream(arr).sum();
+    }
 
     public static void main(String[] args) {
         MultiplyByFive obj = val -> val * 5;
         System.out.println(obj.multBy5(6));
+        AddTwoValues obj1 = (a, b) -> a + b;
+        System.out.println(obj1.addParameters(1879, 456));
+        System.out.println("Sum using streams: " + sumUsingStreams(new int[] { 1, 2, 3, 4, 5 }));
     }
 }
