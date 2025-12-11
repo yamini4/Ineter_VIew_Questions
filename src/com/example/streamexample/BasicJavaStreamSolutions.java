f4f15b3d7fd540e183d06550edc8f0767e4b9148
yamini4 <yamini4.g@gmail.com>
2025-12-11 11:55:43 +0530

    update

--- src/com/example/streamexample/BasicJavaStreamSolutions.java
+++ src/com/example/streamexample/BasicJavaStreamSolutions.java
@@ -2,11 +2,9 @@ package com.example.streamexample;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BasicJavaStreamSolutions {
     public static void main(String[] args) {
@@ -35,7 +33,36 @@ public class BasicJavaStreamSolutions {
         // Sort int[] in descending order
         List<Integer> nums = List.of(5, 12, 3, 19, 7, 1, 25, 9);
         nums.stream().sorted(Comparator.reverseOrder()).forEach(n -> System.out.print(n + " "));
+        System.out.println();
 
-        
+        // Check if any number > 50
+        List<Integer> marks = List.of(10, 20, 35, 42, 18, 55, 47, 67, 89);
+        marks.stream().filter(n -> n > 50).forEach(n -> System.out.print(n + " "));
+        System.out.println();
+
+        // Convert int[] values to their squares
+        int[] digits = { 2, 4, 6, 8, 10 };
+
+        Arrays.stream(digits).map(n -> n * n).forEach(n -> System.out.print(n + " "));
+        System.out.println();
+
+        // Find first element in an int[] stream
+        int[] valuesArr = { 100, 200, 300, 400 };
+        System.out.println(Arrays.stream(valuesArr).findFirst().orElse(0));
+        // peek -
+        Arrays.stream(valuesArr).filter(e -> e > 200).peek(n -> System.out.print(2 * n + " = "))
+                .forEach(System.out::println);
+
+        Stream.of("one", "two", "three", "four", "five")
+                .filter(e -> e.length() > 3)
+                .peek(e -> System.out.println("Filtered value: " + e))
+                .map(String::toUpperCase)
+                .peek(e -> System.out.println("Mapped value: " + e))
+                .collect(Collectors.toList());
+
+        // Count total number of elements in int[]
+        List<Integer> arr = List.of(10, 20, 30, 40, 50, 60);
+        System.out.println(arr.stream().count());
+
     }
 }
