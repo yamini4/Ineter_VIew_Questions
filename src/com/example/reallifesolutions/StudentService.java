98865b91b4af7a740ee4c5316c1e25053fb2bad9
yamini4 <yamini4.g@gmail.com>
2025-12-11 17:48:37 +0530

    updating

---
+++ src/com/example/reallifesolutions/StudentService.java
@@ -0,0 +1,11 @@
+package com.example.reallifesolutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentService {
+
+}
